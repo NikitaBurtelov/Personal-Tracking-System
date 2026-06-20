31646241cf1f0a5b604298371dfde47059970c54
NikitaBurtelov <nikita.burtelov@mail.ru>
2026-06-20 22:20:34 +0300

    fix

--- backend/pts-document-storage-service/src/main/java/org/pts/document/storage/config/mq/RabbitConfig.java
+++ backend/pts-document-storage-service/src/main/java/org/pts/document/storage/config/mq/RabbitConfig.java
@@ -9,8 +9,6 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.DefaultJacksonJavaTypeMapper;
-import org.springframework.amqp.support.converter.JacksonJavaTypeMapper;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
